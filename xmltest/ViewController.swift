8e39b8dcab96943d4a500d4d8d72225628aafa16
Seng96 <senglam199605@gmail.com>
2018-04-10 18:58:41 +0800

    Initial Commit

---
+++ xmltest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xmltest
+//
+//  Created by Seng Lam on 2018/4/10.
+//  Copyright © 2018年 Seng Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
